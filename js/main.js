916788887bc8c7180665e9ab5ee8af1215fd2af6
Matt Chastain <matthewdchastain@gmail.com>
2015-05-20 10:47:03 -0400

    More best practices

--- js/main.js
+++ js/main.js
@@ -15,14 +15,18 @@
 		var currentDate = new Date();
 		displayTime = [currentDate.getHours(), currentDate.getMinutes(), currentDate.getSeconds()];
 		currentSecondsPercentage(displayTime[2]);
-		for (var i = 0; i < displayTime.length; i++) {
-			displayTime[i] < 10 ? displayTime[i] = '0' + displayTime[i] : false;
-		}
+		timeTwoDigits();
 		currentTime = (displayTime[0] + ':' + displayTime[1] + ':' + displayTime[2]);
 		
 		hover === true ? showHexColor() : displayCurrentTime();
 
 	};
+
+	function timeTwoDigits() {
+		for (var i = 0; i < displayTime.length; i++) {
+			displayTime[i] < 10 ? displayTime[i] = '0' + displayTime[i] : false;
+		}
+	}
 
 	window.setInterval(function() {
 			generateCurrentTime();
@@ -36,7 +40,7 @@
 	}
 
 	function displayCurrentTime() {
-		$displayTime.innerHTML = currentTime;
+		$displayTime.textContent = currentTime;
 		hover = false;
 	}
 
@@ -50,7 +54,6 @@
 		colors[1] = ('0' + Number(displayTime[1]).toString(16)).slice(-2);
 		colors[2] = ('0' + Number(displayTime[2]).toString(16)).slice(-2);
 		hexTime = colors[0] + colors[1] + colors[2];
-		console.log(hexTime);
 		return hexTime;
 	}
 
